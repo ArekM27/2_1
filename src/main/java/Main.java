1ca085fb9b7f0490fc1ed289a740f72e5deffd1f
ArekM27 <161622161+ArekM27@users.noreply.github.com>
2024-03-13 15:36:40 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String myString = "Hello world!";
+        for (int i = 0; i < myString.length(); i++) {
+            System.out.println(myString.charAt(i));
+        }
+    }
+}
